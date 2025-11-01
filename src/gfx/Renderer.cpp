5b946f27a1b64d434981c0ffb7903c0588817cbf
mstoyanov7 <92221668+mstoyanov7@users.noreply.github.com>
2025-11-01 20:33:59 +0200

    Utilize Renderer.cpp

--- src/gfx/Renderer.cpp
+++ src/gfx/Renderer.cpp
@@ -1,1 +1,34 @@
 #include "gfx/Renderer.hpp"
+
+void Renderer::Init() 
+{
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_DEPTH_CLAMP);
+    glDepthFunc(GL_LEQUAL);          
+    glClearDepth(1.0);              // default, but be explicit
+}
+
+void Renderer::Clear(float r, float g, float b, float a) 
+{
+    glClearColor(r, g, b, a);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+}
+
+void Renderer::SetWireframe(bool on) 
+{
+    glPolygonMode(GL_FRONT_AND_BACK, on ? GL_LINE : GL_FILL);
+}
+
+void Renderer::SetCull(bool on) 
+{
+    if (on) 
+    { 
+        glEnable(GL_CULL_FACE); 
+        glCullFace(GL_BACK); 
+        glFrontFace(GL_CCW); 
+    }
+    else
+    {
+        glDisable(GL_CULL_FACE);
+    }    
+}
--- src/gfx/Renderer.hpp
+++ src/gfx/Renderer.hpp
@@ -4,36 +4,8 @@
 
 class Renderer {
 public:
-    static void Init() 
-    {
-        glEnable(GL_DEPTH_TEST);
-        glEnable(GL_DEPTH_CLAMP);
-        glDepthFunc(GL_LEQUAL);          
-        glClearDepth(1.0);              // default, but be explicit
-    }
-
-    static void Clear(float r, float g, float b, float a) 
-    {
-        glClearColor(r, g, b, a);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    }
-
-    static void SetWireframe(bool on) 
-    {
-        glPolygonMode(GL_FRONT_AND_BACK, on ? GL_LINE : GL_FILL);
-    }
-
-    static void SetCull(bool on) 
-    {
-        if (on) 
-        { 
-            glEnable(GL_CULL_FACE); 
-            glCullFace(GL_BACK); 
-            glFrontFace(GL_CCW); 
-        }
-        else
-        {
-            glDisable(GL_CULL_FACE);
-        }    
-    }
+    static void Init();
+    static void Clear(float r, float g, float b, float a);
+    static void SetWireframe(bool on);
+    static void SetCull(bool on);
 };
